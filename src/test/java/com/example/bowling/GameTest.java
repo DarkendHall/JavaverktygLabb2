b047c208b43cc4c8a14d94698d5c0a8767de0f0b
DarkendHall <marcus.leeman@iths.se>
2022-01-17 15:08:11 +0100

    Add test for two strikes followed by spare

--- src/test/java/com/example/bowling/GameTest.java
+++ src/test/java/com/example/bowling/GameTest.java
@@ -109,4 +109,21 @@ public class GameTest {
 
         assertThat(result).isEqualTo(42);
     }
+
+    @Test
+    @DisplayName("Checking score after rolling two strike then a spare should add correct bonus")
+    void scoreAfterDoubleStrikeFollowedBySpare() {
+        game.roll(10);
+
+        game.roll(10);
+
+        game.roll(4);
+        game.roll(6);
+
+        game.roll(5);
+
+        var result = game.score();
+
+        assertThat(result).isEqualTo(64);
+    }
 }
