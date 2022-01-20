68f23672db9dda98d31e208eae85d14e7d728777
DarkendHall <marcus.leeman@iths.se>
2022-01-20 11:45:43 +0100

    Add test: Full game with only spares

--- src/test/java/com/example/bowling/GameTest.java
+++ src/test/java/com/example/bowling/GameTest.java
@@ -313,4 +313,43 @@ public class GameTest {
 
         assertThat(result).isEqualTo(300);
     }
+
+    @Test
+    @DisplayName("Full game with only spares should return correct score")
+    void fullGameSparesOnly() {
+        game.roll(7);
+        game.roll(3);
+
+        game.roll(6);
+        game.roll(4);
+
+        game.roll(3);
+        game.roll(7);
+
+        game.roll(1);
+        game.roll(9);
+
+        game.roll(2);
+        game.roll(8);
+
+        game.roll(8);
+        game.roll(2);
+
+        game.roll(3);
+        game.roll(7);
+
+        game.roll(5);
+        game.roll(5);
+
+        game.roll(0);
+        game.roll(10);
+
+        game.roll(1);
+        game.roll(9);
+        game.roll(10);
+
+        var result = game.score();
+
+        assertThat(result).isEqualTo(139);
+    }
 }
